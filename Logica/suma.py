7f00eaea25d9e0c90df36dd16ecddf7c5569ed23
NatiWorlds <natiworlds@gmail.com>
2023-09-30 11:46:47 +0200

    Ejemplo de package en python

---
+++ Logica/suma.py
@@ -0,0 +1,8 @@
+def suma (numeroUno:int, numeroDos:int):
+    numeroUno = int(input("Introduce el primer número: "))
+    numeroDos = int(input("Introduce el primer número: "))
+    return  (f"El resultado de la suma es: {numeroUno+numeroDos}")
+def multiplicacion(numeroUno: int, numeroDos:int):
+    numeroUno = int(input("Introduce el primer número: "))
+    numeroDos = int(input("Introduce el primer número: "))
+    return  (f"El resultado de la multiplicacion es: {numeroUno*numeroDos}")
---
+++ Test_Logica/test_suma.py
@@ -0,0 +1,6 @@
+from Logica.suma import *
+
+def test_suma():
+    assert suma(2, 3) == "El resultado de la suma es: 5"
+def test_multiplicacion():
+    assert multiplicacion(2, 3) == "El resultado de la multiplicacion es: 6"
