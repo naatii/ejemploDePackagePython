64ca1b05978c1b4a565980c1bd3c1301f99ec0ad
naatii <natiworlds@gmail.com>
2023-10-06 11:57:20 +0200

    test como lo quiere edu

--- Test_Logica/test_suma.py
+++ Test_Logica/test_suma.py
@@ -1,7 +1,16 @@
-from src.suma import *
+import pytest
+from src.suma import suma
 
-def test_suma():
-    assert suma(2, 3) == "El resultado de la suma es: 5"
-
-def test_multiplicacion():
-    assert multiplicacion(2, 3) == "El resultado de la multiplicacion es: 6"
+@pytest.mark.parametrize(
+    "input_n1, input_n2, expected",
+    [
+      (1, 1, 2),
+      (0, 0, 0),
+      (100, -100, 0),
+      (-15, -1, -16),
+      (-3, 8, 5),
+      (9, suma(-1, -2), 6)
+    ]
+  )
+def test_suma_params(input_n1, input_n2, expected):
+    assert suma(input_n1, input_n2) == expected
--- src/suma.py
+++ src/suma.py
@@ -1,16 +1,11 @@
-def suma (numeroUno:int, numeroDos:int)->str:
-    return  (f"El resultado de la suma es: {numeroUno+numeroDos}")
-def multiplicacion(numeroUno: int, numeroDos:int)->str:
-    return  (f"El resultado de la multiplicacion es: {numeroUno*numeroDos}")
+def suma (numeroUno:int, numeroDos:int)->int:
+    return  numeroUno + numeroDos 
+
+def salida(resultado:int)-> int:
+    return "El resultado de la suma es ", resultado
 
 if __name__=="__main__":
     # Ejercicio 1
     numeroUnoSuma = int(input("Introduce el primer número: "))
     numeroDosSuma = int(input("Introduce el primer número: "))
-    print(suma(numeroUnoSuma, numeroDosSuma))
-    
-    
-    # Ejercicio 2
-    numeroDosMultiplicacion = int(input("Introduce el primer número: "))
-    numeroUnoMultiplicacion = int(input("Introduce el primer número: "))
-    print(multiplicacion(numeroUnoMultiplicacion, numeroDosMultiplicacion))
+    print(salida(suma(numeroUnoSuma,numeroDosSuma)))
