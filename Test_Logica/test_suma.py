b842575449f6c8afadd840333f7b15e30e8f9369
naatii <natiworlds@gmail.com>
2023-10-07 12:04:34 +0200

    test hechos como edu quiere

--- Test_Logica/test_suma.py
+++ Test_Logica/test_suma.py
@@ -9,7 +9,7 @@ from src.suma import suma
       (100, -100, 0),
       (-15, -1, -16),
       (-3, 8, 5),
-      (9, suma(-1, -2), 6)
+      (9, suma(-1, -2), 6),
     ]
   )
 def test_suma_params(input_n1, input_n2, expected):
